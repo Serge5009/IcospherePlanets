271eb3e97bc5c4f777d4496fa785a2f8c2a9a0c6
Serhii Marchenko <71557129+Serge5009@users.noreply.github.com>
2026-09-05 21:42:51 -0400

    Cleanup

--- Assets/Scripts/BiomeTemplate.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Biome", menuName = "Strategy/Biome Template")]
-public class BiomeTemplate : ScriptableObject
-{
-    public string biomeName;
-    public Color biomeColor = Color.white;
-    public bool isWater;
-}
